db70503d846f3f10e9d5aa2f9d541ddd669647c4
Sam <sam.w@cervus.ai>
2025-03-24 11:31:06 +0000

    Add turn right test

--- src/rover.js
+++ src/rover.js
@@ -1,8 +1,16 @@
 function createRover(position, direction) {
-    return {
-      position: position,
-      direction: direction
-    };
-  }
-  
-  module.exports = createRover;
+  return {
+    position: position,
+    direction: direction,
+  };
+}
+
+function turnRight(rover) {
+  const directions = { N: "E", E: "S", S: "W", W: "N" };
+  return {
+    ...rover,
+    direction: directions[rover.direction],
+  };
+}
+
+module.exports = { createRover, turnRight };
--- test/rover.test.js
+++ test/rover.test.js
@@ -1,13 +1,14 @@
-const createRover = require('../src/rover.js');
+const createRover = require("../src/rover.js");
 
-test('should create rover with position and direction', () => {
-  const rover = createRover([0, 0], 'N');
+test("should create rover with position and direction", () => {
+  const rover = createRover([0, 0], "N");
   expect(rover.position).toEqual([0, 0]);
-  expect(rover.direction).toBe('N');
+  expect(rover.direction).toBe("N");
 });
 
-test('loction slightly different', () => {
- const rover = createRover([0, 0], 'N');
- expect(rover.position).toEqual([2, 1]);
- expect(rover.direction).toBe('E');
-})
+test("should turn right from North to East", () => {
+  const rover = createRover([0, 0], "N");
+  const newRover = turnRight(rover);
+  expect(newRover.direction).toBe("E");
+  expect(newRover.position).toEqual([0, 0]);
+});
